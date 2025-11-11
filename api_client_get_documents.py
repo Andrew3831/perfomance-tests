708322de45f32ba8642f41e4e4cf5671c94fc497
Andrew3831 <andreyandr142@gmail.com>
2025-11-11 12:57:55 +0300

    Применяем Pydantic в HTTP API клиентах

--- api_client_get_documents.py
+++ api_client_get_documents.py
@@ -12,14 +12,14 @@ create_user_response = users_gateway_client.create_user()
 print('Create user response:', create_user_response)
 
 # Извлекаем user_id
-user_id = create_user_response['user']['id']
+user_id = create_user_response.user.id
 
 # Шаг 2: Открываем кредитный счет
 open_credit_card_account_response = accounts_gateway_client.open_credit_card_account(user_id)
 print('Open credit card account response:', open_credit_card_account_response)
 
 # Извлекаем account_id
-account_id = open_credit_card_account_response['account']['id']
+account_id = open_credit_card_account_response.account.id
 
 # Шаг 3: Получаем тарифный документ
 get_tariff_document_response = documents_gateway_client.get_tariff_document(account_id)
--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,26 +1,31 @@
-from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, Field, EmailStr
 
 
-# Модель данных пользователя
 class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
     id: str
     email: EmailStr
-    last_name: str
-    first_name: str
-    middle_name: Optional[str] = None
-    phone_number: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
 
 
-# Запрос на создание пользователя
 class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
     email: EmailStr
-    last_name: str
-    first_name: str
-    middle_name: Optional[str] = None
-    phone_number: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
 
 
-# Ответ с данными созданного пользователя
 class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
     user: UserSchema
